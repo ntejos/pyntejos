b46f94379b093bb754b624e1c0fa6c4840278362
Nicolas Tejos <ntejos@gmail.com>
2016-03-16 20:10:37 -0600

    minor tweak

--- pyntejos/for_risa/cross_match_cpairs_qsos_config.py
+++ pyntejos/for_risa/cross_match_cpairs_qsos_config.py
@@ -11,10 +11,10 @@ mag_fuv_min = 19.0, # Minimum magnitude in the FUV
 
 #Parameters for cluster-pairs
 richmin  = 20, # minimum richness
-max_tsep = 25, # (in Mpc) maximum transverse separation between the clusters.
+max_tsep = 20, # (in Mpc) maximum transverse separation between the clusters.
 
 #Parameters for the cross-match
-max_dsep = 5,     # (in Mpc) maximum impact parameter between the cluster pair axis and the QSO sightline.
+max_dsep = 3,     # (in Mpc) maximum impact parameter between the cluster pair axis and the QSO sightline.
 min_xsep = 3,     # (in Mpc) minimum distance between the closest cluster and the QSO sightline.
 dv_group = 1000,  # (in km/s) velocity for grouping cluster-pairs as an independent system
 vel_prox = 3000,  # (in km/s) velocity window for excluding zones close to the QSOs systemic redshift.
