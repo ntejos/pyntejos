e8d54d181e386ea2771d66599fc791e0a085dee7
Nicolas Tejos <ntejos@gmail.com>
2019-01-31 09:08:22 -0300

    fixing indexing for boxen in slit (1 being towards north)

--- pyntejos/regions.py
+++ pyntejos/regions.py
@@ -3,8 +3,8 @@ from astropy import units as u
 """Module to handle DS9 regions utilities"""
 
 
-def get_boxregion_string(x, y, a, b, angle):
-    """Returns a ds9 string associated to a box with position (x,y) and PA
+def get_boxregion_string(x, y, a, b, angle, text=None):
+    """Returns a ds9 string associated to a box with position (x,y) and angle
     Parameters
     ----------
     x : float
@@ -17,13 +17,15 @@ def get_boxregion_string(x, y, a, b, angle):
         Size of the box in the y-coordinates at PA=0 (pixel units)
     angle : float
         angle of the box for ds9
+    text : str
+        A comment for the region
 
     """
-    s = 'box({:.8f},{:.8f},{},{},{:.8f}) ||\n'.format(x, y, a, b, angle)
+    s = 'box({:.8f},{:.8f},{},{},{:.8f}) # text={{{}}}\n'.format(x, y, a, b, angle, text)
     return s
 
 
-def create_regfile_nboxes_from_slit(output, xy, PA, l, w, n, pixscale):
+def create_regfile_nboxes_from_slit(output, xy, PA, l, w, n, pixscale, add_label=True):
     """
 
     Based on F. Corro script
@@ -46,6 +48,8 @@ def create_regfile_nboxes_from_slit(output, xy, PA, l, w, n, pixscale):
         Odd integer
     pixscale : float
         Pixel scale in arcsec
+    add_label : bool
+        Whether to add a label to the region, (1 to n)
 
     Returns
     -------
@@ -67,9 +71,12 @@ def create_regfile_nboxes_from_slit(output, xy, PA, l, w, n, pixscale):
     angle =  PA  # angle in x-y coordinates is offset from PA
     angle_rad = angle * np.pi / 180.
     for ii in range(-1*nside, nside+1):
-        xnew = xy[0] - ii * b * np.sin(angle_rad)
-        ynew = xy[1] + ii * b * np.cos(angle_rad)
-        s = get_boxregion_string(xnew, ynew, a, b, angle)
+        xnew = xy[0] + ii * b * np.sin(angle_rad)
+        ynew = xy[1] - ii * b * np.cos(angle_rad)
+        text = None
+        if add_label:
+            text = str(ii+nside+1)
+        s = get_boxregion_string(xnew, ynew, a, b, angle, text=text)
         f.write(s)
     f.close()
 
