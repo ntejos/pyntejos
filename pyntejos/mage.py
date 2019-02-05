aa5c99a89beae74c1025763de7503c9940b3f218
Nicolas Tejos <ntejos@gmail.com>
2019-02-05 17:39:27 -0300

    add all chi2 in tables

--- pyntejos/mage.py
+++ pyntejos/mage.py
@@ -510,6 +510,7 @@ def determine_best_astrometry(magecube_filename, musecube_filename, xc_array, yc
     # now read the cubes and perform the chi2
     chi2 = []
     chi2w = []
+    chi2_11 = []
     for name in tab['name']:
         newcube_name = master_dirname + '/' + name + '/magecube_from_muse_{}.fits'.format(name)
         # load the magecube
@@ -562,9 +563,11 @@ def determine_best_astrometry(magecube_filename, musecube_filename, xc_array, yc
         print("Weighted Chi2/DOF = {:.1f}".format(np.sum(chi2_w)))
         chi2 += [np.sum(chi2_tot)/n]
         chi2w += [np.sum(chi2_w)]
+        chi2_11 += [chi2_tot]
     tab['chi2'] = chi2
     tab['chi2w'] = chi2w
-    tab.write(master_dirname+'/astrometry_results.dat', format='ascii', overwrite=True)
-    tab.write(master_dirname+'/astrometry_results.fits', overwrite=True)
+    tab['chi2_11'] = chi2_11
+    tab.write(master_dirname + '/astrometry_results.dat', format='ascii', overwrite=True)
+    tab.write(master_dirname + '/astrometry_results.fits', overwrite=True)
     return tab
 
